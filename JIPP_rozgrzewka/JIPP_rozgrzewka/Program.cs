c5492327da8966b8e92911c134d8515dfb6ac893
Mateusz Cieśla <115277907+9831MC@users.noreply.github.com>
2023-01-09 20:11:00 +0100

    Add files via upload

---
+++ JIPP_rozgrzewka/JIPP_rozgrzewka/Program.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JIPP_rozgrzewka
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {   //Pierwszy program rozgrzewka
+            //Zadanie 2
+            //Console.WriteLine("Podaj swoje imię:");
+            //var name = Console.ReadLine();
+            //Console.WriteLine("Hello " + name);
+
+            //Zadanie 3
+            //int result1 = 5 + 9;
+
+            //Operatory
+            //Zadanie 1
+            //int number = 123;
+            //float money = 23.42676f;
+            //string text = "Przykładowy tekst";
+            //bool isLogged = true;
+            //char myChar = 'k';
+            //decimal price = 43.45m;
+            //Console.WriteLine(number);
+            //Console.WriteLine(money);
+            //Console.WriteLine(text);
+            //Console.WriteLine(isLogged);
+            //Console.WriteLine(myChar);
+            //Console.WriteLine(price);
+
+            //Zadanie 2
+            //string myAge = "Age: ";
+            //int wifeAge = 18;
+            //string result = myAge + wifeAge;
+            //Console.WriteLine(result);
+            // wniosek: string automatycznie przekonwertował zmienną typu cąłkowitego na ciąg znakowy.
+
+            //zadanie 3
+            //bool isTrue = true;
+            //bool isFalse = false;
+            //bool isReallyTrue = true;
+            //string and = isTrue + " AND " + isFalse;
+            //string or = isTrue + " OR " + isReallyTrue;
+            //string negative = Convert.ToString(isFalse);
+            //Console.WriteLine(and);
+            //Console.WriteLine(or);
+            //Console.WriteLine(negative);
+
+            //Zadanie 4
+            //int a = 5;
+            //int b = 12;
+            //int add = a + b;
+            //int sub = a - b;
+            //float div = a / b;
+            //int mul = a * b;
+            //float mod = b % a;
+            //Console.WriteLine(add);
+            //Console.WriteLine(sub);
+            //Console.WriteLine(div);
+            //Console.WriteLine(mul);
+            //Console.WriteLine(mod);
+
+            //zadanie 5
+            //string a = "Ala ";
+            //string b = "ma ";
+            //string c = "kota.";
+            //string result = a + b + c;
+            //Console.WriteLine(result);
+            // wniosek: operator '+' może służyć nie tylko do dodawania ze sobą liczb ale też do łączenia ciągów znakowych.
+
+            //instrukcje sterujące i pętle
+            //Zadanie 1
+            //int n1 = 10;
+            //int n2 = 20;
+            //if (n1 > 20)
+            //{
+            //    Console.WriteLine("n1 jest większe od n2");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("n1 jest równe n2"); //to jest logicznie nie poprawne. trzeba zrobić trzeci wariant - wtedy gdy n2 jest większe od n1.
+            //}
+
+            //Zadanie 2
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("C#");
+            //}
+
+            //zadanie 3
+            //int n = 10;
+            //for(int i=0; i <= n; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i + " - Parzysta");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(i + " - Nieparzysta");
+            //    }
+            //}
+
+            //zadanie 4
+            //int n = 5;
+
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("* ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //zadanie 5
+            //int exam = 101;
+
+            //if (exam >= 0 && exam <= 39)
+            //{
+            //    Console.WriteLine("Ocena Niedostateczna");
+            //}
+            //else if (exam >= 40 && exam <= 54)
+            //{
+            //    Console.WriteLine("Ocena Dopuszczająca");
+            //}
+            //else if (exam >= 55 && exam <= 69)
+            //{
+            //    Console.WriteLine("Ocena Dostateczna");
+            //}
+            //else if (exam >= 70 && exam <= 84)
+            //{
+            //    Console.WriteLine("Ocena Dobra");
+            //}
+            //else if (exam >= 85 && exam <= 98)
+            //{
+            //    Console.WriteLine("Ocena Bardzo Dobra");
+            //}
+            //else if (exam >= 99 && exam <= 100)
+            //{
+            //    Console.WriteLine("Ocena Celująca");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Wartość poza zakresem");
+            //}
+
+            //kolekcje
+            //Zadanie 1
+            //string[] colors = { "niebieski", "zielony", "żółty", "czerwony" };
+
+            //Console.WriteLine($"Mój pierwszy kolor to: {colors[0]}");
+            //Console.WriteLine($"Mój ostatni kolor to: {colors[colors.Length - 1]}");
+
+            //Zadanie 2
+            //int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("Liczba: " + numbers[i]);
+            //}
+            //Console.WriteLine("");
+            //foreach (int number in numbers)
+            //{
+            //    Console.WriteLine("Liczba: " + number);
+            //}
+            //Console.WriteLine("");
+            //int j = 0;
+            //while (j < 10)
+            //{
+            //    Console.WriteLine("Liczba: " + numbers[j]);
+            //    j++;
+            //}
+
+            //zadanie 3
+            //List<string> fruits = new List<string>();
+
+            //fruits.Add("Pomidor");
+            //fruits.Add("Jabłko");
+            //fruits.Add("Marchewka");
+
+            //foreach (string fruit in fruits)
+            //{
+            //    Console.Write(fruit + ", ");
+            //}
+
+            //fruits.Remove("Pomidor");
+            //fruits.RemoveAt(fruits.Count - 1);
+            //Console.WriteLine("\nLista po usunięciu elementów:");
+            //foreach (string fruit in fruits)
+            //{
+            //    Console.WriteLine(fruit);
+            //}
+
+            //Warsztat
+            //Zadanie 1
+        }
+    }
+}
